5674ec8e5cbdcc0696ebc497cdb95d3c65e56536
Desmond Herring <desmond.m.herring.mil@army.mil>
2024-08-21 11:57:21 -0500

    Changes ive made messin around in class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,21 @@
+import javax.swing.*;
 
 public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        JButton button = new JButton("Click Me");
+
+        panel.add(button);
+
+        frame.setVisible(true);
+    }
 
 }
